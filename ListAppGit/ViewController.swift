9abe86db2a409852f00145bd550ca4d03ba60ebe
cristianpalage <cristianpalage@gmail.com>
2020-08-23 23:02:05 -0400

    Initial Commit

---
+++ ListAppGit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListAppGit
+//
+//  Created by Cristian Palage on 2020-08-23.
+//  Copyright © 2020 Cristian Palage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
