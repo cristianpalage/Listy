28e7b82bc72c4163e7ed39cac35a61ed4542e92e
Cristian Palage <cristianpalage@gmail.com>
2020-10-20 00:16:37 -0400

    bug fix

--- ListAppGit/UserInterface/Settings/SettingsTableView.swift
+++ ListAppGit/UserInterface/Settings/SettingsTableView.swift
@@ -241,14 +241,14 @@ extension SettingsTableView {
 extension SettingsTableView: Themed {
     func applyTheme(_ theme: AppTheme) {
         self.tableView.backgroundColor = theme.secondaryBackgroundColor
+        navigationItem.leftBarButtonItem?.tintColor = theme.textColor
+        navigationItem.backBarButtonItem?.tintColor = theme.tintColor
 
         navigationItem.leftBarButtonItem?.setTitleTextAttributes([
             NSAttributedString.Key.font: fontProvider.currentFont.fontValue().withSize(17),
             NSAttributedString.Key.foregroundColor: theme.textColor
         ],
         for: .normal)
-        navigationItem.leftBarButtonItem?.tintColor = theme.textColor
-
         navigationItem.backBarButtonItem?.setTitleTextAttributes([
             NSAttributedString.Key.font: fontProvider.currentFont.fontValue().withSize(17),
             NSAttributedString.Key.foregroundColor: theme.textColor
@@ -264,13 +264,14 @@ extension SettingsTableView: FontProtocol {
             NSAttributedString.Key.foregroundColor: self.themeProvider.currentTheme.textColor
         ],
         for: .normal)
-        navigationItem.leftBarButtonItem?.tintColor = self.themeProvider.currentTheme.textColor
 
         navigationItem.backBarButtonItem?.setTitleTextAttributes([
             NSAttributedString.Key.font: font.fontValue().withSize(17),
             NSAttributedString.Key.foregroundColor: self.themeProvider.currentTheme.textColor
         ],
         for: .normal)
+
+        navigationItem.leftBarButtonItem?.tintColor = self.themeProvider.currentTheme.textColor
         navigationItem.backBarButtonItem?.tintColor = self.themeProvider.currentTheme.tintColor
     }
 }
